65a80a959ea71a2b2595fc70394885ee6cdd8215
Shone Appasaheb Pansambal <64965184+Shone2402@users.noreply.github.com>
2021-07-19 11:42:07 +0530

    Update settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -24,9 +24,9 @@ TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-=dosztlx(ayhi)&(5a0gai45wsr%ih%gcc!d8fvvd(2v6nm@1n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','socialmedia2402.herokuapp.com/']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
